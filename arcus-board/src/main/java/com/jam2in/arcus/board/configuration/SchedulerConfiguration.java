f4133bdedce84ee4657cdc2b19b604c314474f0c
kohseongyeon <kohoct@gmail.com>
2021-06-15 12:06:24 +0900

    add Scheduler Thread Pool

---
+++ arcus-board/src/main/java/com/jam2in/arcus/board/configuration/SchedulerConfiguration.java
@@ -0,0 +1,22 @@
+package com.jam2in.arcus.board.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+@Configuration
+public class SchedulerConfiguration implements SchedulingConfigurer {
+	private static final int THREAD_POOL_SIZE = 2;
+
+	@Override
+	public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+		ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+
+		threadPoolTaskScheduler.setPoolSize(THREAD_POOL_SIZE);
+		threadPoolTaskScheduler.setThreadNamePrefix("scheduled-task-pool-");
+		threadPoolTaskScheduler.initialize();
+
+		taskRegistrar.setTaskScheduler(threadPoolTaskScheduler);
+	}
+}
